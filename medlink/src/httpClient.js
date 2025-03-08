fdae4a1dc89a2d16a7d3cfc422b60c581020e729
praveenpiske <piskepraveen@gmail.com>
2025-03-08 17:55:09 +0530

    updated: deployment server link

--- medlink/src/httpClient.js
+++ medlink/src/httpClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = (process.env.NODE_ENV === 'development' ? "http://127.0.0.1:5000" : "");
+const url = (process.env.NODE_ENV === 'development' ? "http://127.0.0.1:5000" : "https://medlink-965q.onrender.com");
 
 export default axios.create({
   withCredentials: true,
